52b4c40da77dc106375784469a878490ae5fa1b7
super <supeng842499467@gmail.com>
2021-01-09 10:11:26 +0800

    update

--- 6. 数组/code/11.go
+++ 6. 数组/code/11.go
@@ -7,5 +7,27 @@ package code
 **/
 
 func maxArea(height []int) int {
+	if height == nil || len(height) == 0 {
+		return 0
+	}
+	maxArea, left, right := 0, 0, len(height)-1
+	for left < right {
+		area := (right - left) * min(height[left], height[right])
+		if area > maxArea {
+			maxArea = area
+		}
+		if height[left] > height[right] {
+			right--
+		} else {
+			left++
+		}
+	}
+	return maxArea
+}
 
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
 }
