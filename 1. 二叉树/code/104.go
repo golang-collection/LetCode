998a9f13c38fc1c65383c850c7a0c319585442f2
super <supeng842499467@gmail.com>
2021-01-22 21:56:57 +0800

    update

---
+++ 1. 二叉树/code/104.go
@@ -0,0 +1,21 @@
+package code
+
+/**
+* @Author: super
+* @Date: 2021-01-22 21:28
+* @Description:
+**/
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
